14ebf7b49c14fe76ba0d6d10f6938828e431d25a
Renan Silva <renansilvaytB@gmail.com>
2025-01-20 08:26:05 -0300

    feat: finish experience bar

--- src/components/experience-bar.tsx
+++ src/components/experience-bar.tsx
@@ -1,10 +1,12 @@
+import style from '../styles/components/experienceBar.module.css'
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={style.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '40%' }}></div>
-        <span className="current-experience" style={{ left: '40%' }}>300 xp</span>
+        <span className={style.currentExperience} style={{ left: '40%' }}>300 xp</span>
       </div>
       <span>600 xp</span>
     </header>
